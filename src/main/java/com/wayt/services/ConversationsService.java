f71722f449adf6ae44f9361a930a8512d6b4b16a
pragya <saxena.pragya8@gmail.com>
2017-01-31 23:15:18 -0500

    generating slug from subject name

--- src/main/java/com/wayt/services/ConversationsService.java
+++ src/main/java/com/wayt/services/ConversationsService.java
@@ -3,6 +3,7 @@ package com.wayt.services;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.security.auth.Subject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -22,8 +23,9 @@ public class ConversationsService {
 	@Path("/addconversation")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public UpdateStatusResponse addConversation(@QueryParam(value = "userId")int usrId, @QueryParam(value = "subject") String sub, @QueryParam(value = "link") String articleLink, @QueryParam(value = "slug") String slug, @QueryParam(value = "recipientIds") List<String> recipients, @QueryParam(value = "content")String content) throws SQLException, ClassNotFoundException {
+	public UpdateStatusResponse addConversation(@QueryParam(value = "userId")int usrId, @QueryParam(value = "subject") String sub, @QueryParam(value = "link") String articleLink, @QueryParam(value = "recipientIds") List<String> recipients, @QueryParam(value = "content")String content) throws SQLException, ClassNotFoundException {
 		List<Integer> recipientIds = UserDao.getInstance().getAllUserIdsForEmails(recipients);
+		String slug = sub.replaceAll(" ", "-");
 		boolean status = ConversationsDao.getInstance().addNewConversation(usrId, sub, articleLink, slug, recipientIds, content);
 		return new UpdateStatusResponse(status);
 	}
